7b70055236d386101fb8d241fdaef20cdc9e042e
Fadli <fadliafandi20@gmail.com>
2025-07-30 11:12:08 +0700

    NewUpdate1

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Katalog Produk',
+  description: 'Created with Sayaa',
+  generator: 'Sayaa.dev',
 }
 
 export default function RootLayout({
